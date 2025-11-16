863333c62f22250c20749a46a69ad8bb1cb408b8
Marcus Rådell <marcus@radell.net>
2025-11-16 13:05:46 +0100

    break out authorization authorize to its own file

---
+++ src/features/authorization/authorize.rs
@@ -0,0 +1,21 @@
+use axum::{Form, response::Redirect};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct AuthorizeFormData {
+    email: String,
+    password: String,
+}
+
+pub async fn authorize(
+    Form(body): Form<AuthorizeFormData>,
+) -> Result<Redirect, axum::http::StatusCode> {
+    let expected_email = std::env::var("SIGN_IN_EMAIL").unwrap();
+    let expected_password = std::env::var("SIGN_IN_PASSWORD").unwrap();
+
+    if body.email != expected_email || body.password != expected_password {
+        return Err(axum::http::StatusCode::UNAUTHORIZED);
+    }
+
+    Ok(Redirect::to("/client/authorization_callback?code=123"))
+}
--- src/features/authorization/mod.rs
+++ src/features/authorization/mod.rs
@@ -1,30 +1,11 @@
+mod authorize;
 mod sign_in;
 
 use axum::{
-    Form, Router,
-    response::{IntoResponse, Redirect},
+    Router,
+    response::IntoResponse,
     routing::{get, post},
 };
-use serde::Deserialize;
-
-#[derive(Deserialize)]
-struct AuthorizeFormData {
-    email: String,
-    password: String,
-}
-
-async fn authorize(
-    Form(body): Form<AuthorizeFormData>,
-) -> Result<Redirect, axum::http::StatusCode> {
-    let expected_email = std::env::var("SIGN_IN_EMAIL").unwrap();
-    let expected_password = std::env::var("SIGN_IN_PASSWORD").unwrap();
-
-    if body.email != expected_email || body.password != expected_password {
-        return Err(axum::http::StatusCode::UNAUTHORIZED);
-    }
-
-    Ok(Redirect::to("/client/authorization_callback?code=123"))
-}
 
 async fn token() -> impl IntoResponse {
     "[[TODO: token string]]"
@@ -33,6 +14,6 @@ async fn token() -> impl IntoResponse {
 pub fn router() -> Router {
     Router::new()
         .route("/sign_in", get(sign_in::sign_in))
-        .route("/authorize", post(authorize))
+        .route("/authorize", post(authorize::authorize))
         .route("/token", post(token))
 }
