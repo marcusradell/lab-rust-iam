402d696c8f63296574869af44f5b88b385686e51
Marcus Rådell <marcus@radell.net>
2025-11-16 14:24:56 +0100

    add route test for authorization authorize

--- src/features/authorization/router_test.rs
+++ src/features/authorization/router_test.rs
@@ -1,6 +1,6 @@
 use axum::{
     body::Body,
-    http::{Request, StatusCode},
+    http::{Request, StatusCode, header},
 };
 use tower::ServiceExt;
 
@@ -19,4 +19,33 @@ async fn test_sign_in_route() {
         .unwrap();
 
     assert_eq!(response.status(), StatusCode::OK)
+}
+
+#[tokio::test]
+async fn test_authorize() {
+    dotenvy::from_filename("test.env").unwrap();
+    let router = super::router();
+
+    let form_data = form_urlencoded::Serializer::new(String::new())
+        .append_pair("email", &std::env::var("SIGN_IN_EMAIL").unwrap())
+        .append_pair("password", &std::env::var("SIGN_IN_PASSWORD").unwrap())
+        .finish();
+
+    let response = router
+        .oneshot(
+            Request::builder()
+                .uri("/authorize")
+                .method("POST")
+                .header(header::CONTENT_TYPE, "application/x-www-form-urlencoded")
+                .body(Body::from(form_data))
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::SEE_OTHER);
+    assert_eq!(
+        response.headers().get("location").unwrap(),
+        "http://localhost:3000/client/authorization_callback?code=123"
+    )
 }
